9f8a7416c18b8cf9031bbcad648fc55d035ce8ab
Harry Ganz <hganz@harryganz.com>
2024-01-04 21:40:12 -0500

    Random movement boids

--- boids.js
+++ boids.js
@@ -1,107 +1,71 @@
 // Globals
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var avoidFactor = 0.1;
-var matchingFactor = 0.1;
-var centeringFactor = 0.01;
-var avoidDist = 10;
-var maxSpeed = 50;
-var minSpeed = 5;
 var x = 0;
 var y = Math.floor(canvas.height / 2);
 var Boid = /** @class */ (function () {
-    function Boid(x, y) {
-        if (x === void 0) { x = 0; }
-        if (y === void 0) { y = 0; }
+    function Boid(x, y, canvasOpts, opts) {
+        var _a = opts || {}, vx = _a.vx, vy = _a.vy, size = _a.size, color = _a.color;
         this.x = x;
         this.y = y;
-        this.vx = 0;
-        this.vy = 0;
+        this.canvasOpts = canvasOpts;
+        this.vx = vx || 0;
+        this.vy = vy || 0;
+        this.size = size || 10;
+        this.color = color || "#000000";
     }
-    Boid.prototype.update = function (neighbors) {
-        this.separate(neighbors);
-        this.align(neighbors);
-        this.cohere(neighbors);
-        // Clamp speed
-        var speed = Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2));
-        if (speed > maxSpeed) {
-            this.vx = (this.vx / speed) * maxSpeed;
-            this.vy = (this.vy / speed) * maxSpeed;
-        }
-        if (speed < minSpeed) {
-            this.vx = (this.vx / speed) * minSpeed;
-            this.vy = (this.vy / speed) * minSpeed;
-        }
-        this.x += this.vx;
-        this.y += this.vy;
+    Boid.prototype.draw = function (ctx) {
+        // Creates a circle
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.closePath();
+        ctx.fill();
     };
-    Boid.prototype.separate = function (neighbors) {
-        var _this = this;
-        var close_dx = 0;
-        var close_dy = 0;
-        neighbors.filter(function (neighbor) { return dist(_this.x, neighbor.x, _this.y, neighbor.y) < avoidDist; }).forEach(function (neighbor) {
-            close_dx += _this.x - neighbor.x;
-            close_dy += _this.y - neighbor.y;
-        });
-        this.vx += close_dx * avoidFactor;
-        this.vy += close_dy * avoidFactor;
-    };
-    Boid.prototype.align = function (neighbors) {
-        var _a = [0, 0, 0], xvel_avg = _a[0], yvel_avg = _a[1], neighbor_cnt = _a[2];
-        neighbors.forEach(function (neighbor) {
-            xvel_avg += neighbor.vx;
-            yvel_avg += neighbor.vy;
-            neighbor_cnt += 1;
-        });
-        if (neighbor_cnt > 0) {
-            xvel_avg = xvel_avg / neighbor_cnt;
-            yvel_avg = yvel_avg / neighbor_cnt;
+    Boid.prototype.update = function (dt) {
+        var _a = this.canvasOpts, width = _a.width, height = _a.height;
+        if ((this.x + this.vx - this.size) < 0 || (this.x + this.vx + this.size) > width) {
+            this.vx = -1 * this.vx;
         }
-        this.vx += (xvel_avg - this.vx) * matchingFactor;
-        this.vy = (yvel_avg - this.vy) * matchingFactor;
-    };
-    Boid.prototype.cohere = function (neighbors) {
-        var _a = [0, 0, 0], xpos_avg = _a[0], ypos_avg = _a[1], neighbor_cnt = _a[2];
-        neighbors.forEach(function (neighbor) {
-            xpos_avg += neighbor.x;
-            ypos_avg += neighbor.y;
-            neighbor_cnt += 1;
-        });
-        if (neighbor_cnt > 0) {
-            xpos_avg = xpos_avg / neighbor_cnt;
-            ypos_avg = ypos_avg / neighbor_cnt;
+        if ((this.y + this.vy - this.size) < 0 || (this.y + this.vy + this.size) > height) {
+            this.vy = -1 * this.vy;
         }
-        this.vx += (xpos_avg - this.x) * centeringFactor;
-        this.vy += (ypos_avg - this.y) * centeringFactor;
+        this.x += this.vx * dt / 1000;
+        this.y += this.vy * dt / 1000;
     };
     return Boid;
 }());
-function dist(x1, x2, y1, y2) {
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-}
+var boids = [];
 /**
  * Initializes the canvas. Should only run on page load.
  */
 function init() {
-    if (ctx != null) {
-        ctx.arc(x, y, 10, 0, 2 * Math.PI);
-        ctx.fill();
+    if (canvas == null || ctx == null)
+        return;
+    // Create 10 boids
+    for (var i = 0; i < 10; i++) {
+        var boid = new Boid(Math.random() * canvas.width - 10, Math.random() * canvas.height - 10, { width: canvas.width, height: canvas.height }, { vx: Math.random() * 100 - 50, vy: Math.random() * 100 - 50 });
+        boids.push(boid);
+        boid.draw(ctx);
     }
 }
 var previousTimestamp;
 /** Creates a step of animation
  */
 function step(timestamp) {
+    if (ctx == null)
+        return;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
     if (previousTimestamp === undefined) {
         previousTimestamp = timestamp;
     }
     var dt = timestamp - previousTimestamp; // dt in milliseconds
-    // Set x velocity to 30px per second
-    var dx = 30 * dt / 1000;
-    x += dx;
-    if (x < canvas.width) {
-        window.requestAnimationFrame(step);
-    }
+    boids.forEach(function (boid) {
+        boid.update(dt);
+        boid.draw(ctx);
+    });
+    previousTimestamp = timestamp;
+    window.requestAnimationFrame(step);
 }
 window.onload = function () {
     init();
--- boids.ts
+++ boids.ts
@@ -2,124 +2,95 @@
 // Globals
 const canvas : HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-const avoidFactor = 0.1;
-const matchingFactor = 0.1;
-const centeringFactor = 0.01;
-const avoidDist = 10;
-const maxSpeed = 50;
-const minSpeed = 5;
 
 
 let x = 0;
 const y = Math.floor(canvas.height/2);
 
-class Boid {
+interface Drawable {
+	draw: (ctx: CanvasRenderingContext2D) => void
+}	
+
+type CanvasOpts = {
+	width: number
+	height: number
+}
+class Boid implements Drawable {
 	x: number
 	y: number
 	vx: number
 	vy: number
-
-
-	constructor(x: number = 0, y: number = 0) {
+	private size: number
+	private color: string
+	private canvasOpts: CanvasOpts
+	constructor(x: number, y: number, canvasOpts: CanvasOpts, opts?: {vx?: number, vy?: number, size?: number, color?: string} ) {
+		const {vx, vy, size, color} = opts || {};
 		this.x = x;
 		this.y = y;
-		this.vx = 0;
-		this.vy = 0;
+		this.canvasOpts = canvasOpts;
+		this.vx = vx || 0;
+		this.vy = vy || 0;
+		this.size = size || 10;
+		this.color = color || "#000000";
 	}
 
-	update(neighbors: Boid[]) {
-		this.separate(neighbors);
-		this.align(neighbors);
-		this.cohere(neighbors);
-		// Clamp speed
-		const speed = Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2));
-		if (speed > maxSpeed) {
-			this.vx = (this.vx/speed)*maxSpeed;
-			this.vy = (this.vy/speed)*maxSpeed;
+	draw(ctx: CanvasRenderingContext2D): void {
+		// Creates a circle
+		ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+		ctx.fillStyle = this.color;
+		ctx.closePath();
+		ctx.fill();
+	}
+
+	update(dt: number): void {
+		const {width, height} = this.canvasOpts;
+		if ((this.x + this.vx - this.size) < 0 || (this.x + this.vx + this.size) > width) {
+			this.vx = -1*this.vx;
 		}
-		if (speed < minSpeed) {
-			this.vx = (this.vx/speed)*minSpeed;
-			this.vy = (this.vy/speed)*minSpeed;
+		if ((this.y + this.vy - this.size) < 0 || (this.y + this.vy + this.size) > height) {
+			this.vy = -1*this.vy;
 		}
-
-		this.x += this.vx;
-		this.y += this.vy;
+		this.x += this.vx * dt/1000;
+		this.y += this.vy * dt/1000;
 	}
-
-	private separate(neighbors: Boid[]) : void {
-		let close_dx = 0;
-		let close_dy = 0;
-		neighbors.filter(neighbor => dist(this.x, neighbor.x, this.y, neighbor.y) < avoidDist).forEach(neighbor => {
-			close_dx += this.x - neighbor.x;
-			close_dy += this.y - neighbor.y;
-		});
-		this.vx += close_dx*avoidFactor;
-		this.vy += close_dy*avoidFactor;
-	}
-
-	private align(neighbors: Boid[]) : void {
-		let [xvel_avg, yvel_avg, neighbor_cnt] = [0, 0, 0]
-		neighbors.forEach(neighbor => {
-			xvel_avg += neighbor.vx;
-			yvel_avg += neighbor.vy;
-			neighbor_cnt += 1;
-		});
-		if (neighbor_cnt > 0) {
-			xvel_avg = xvel_avg/neighbor_cnt;
-			yvel_avg = yvel_avg/neighbor_cnt;
-		}
-		this.vx += (xvel_avg - this.vx)*matchingFactor;
-		this.vy = (yvel_avg - this.vy)*matchingFactor; 
-	}
-
-	private cohere(neighbors: Boid[]) : void {
-		let [xpos_avg, ypos_avg, neighbor_cnt] = [0, 0, 0];
-
-		neighbors.forEach(neighbor => {
-			xpos_avg += neighbor.x;
-			ypos_avg += neighbor.y;
-			neighbor_cnt += 1;
-		});
-
-		if (neighbor_cnt > 0) {
-			xpos_avg = xpos_avg/neighbor_cnt;
-			ypos_avg = ypos_avg/neighbor_cnt;
-		}
-
-		this.vx += (xpos_avg - this.x)*centeringFactor;
-		this.vy += (ypos_avg - this.y)*centeringFactor;
-	}
-
-	
-}
-
-function dist(x1: number, x2: number, y1: number, y2: number): number {
-	return Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
-}
+} 
+const boids: Boid[] = [];
 /**
  * Initializes the canvas. Should only run on page load.
  */
 function init() : void {
-	if (ctx != null) {
-		ctx.arc(x,y, 10, 0, 2*Math.PI)
-		ctx.fill();
-	}
+	if (canvas == null || ctx == null) return;
+	// Create 10 boids
+	for (let i = 0; i < 10; i++) {
+		const boid = new Boid(Math.random()*canvas.width - 10, Math.random()*canvas.height - 10, {width: canvas.width, height: canvas.height}, {vx: Math.random()*100 - 50, vy: Math.random()*100 - 50}); 
+		boids.push(boid);
+		boid.draw(ctx);
+	} 
+
 }
 
 let previousTimestamp : number | undefined;
 /** Creates a step of animation
  */
 function step(timestamp: number) : void {
+	if (ctx == null) return;
+
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+	ctx.beginPath();
+
 	if (previousTimestamp === undefined) {
 		previousTimestamp = timestamp;
 	}
+
 	const dt = timestamp - previousTimestamp // dt in milliseconds
-	// Set x velocity to 30px per second
-	const dx = 30 * dt/1000
-	x += dx;
-	if (x < canvas.width) {
-		window.requestAnimationFrame(step);
-	}
+	boids.forEach(boid => {
+		boid.update(dt);
+		boid.draw(ctx);
+	});
+
+	previousTimestamp = timestamp;
+	
+	window.requestAnimationFrame(step);
 } 
 
 window.onload = () => {
