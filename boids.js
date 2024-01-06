3e6e6743f3afa0e6e0c2583400a823ba2a80f65a
Harry Ganz <hganz@harryganz.com>
2024-01-06 17:24:53 -0500

    Clamp speed to limits

--- boids.js
+++ boids.js
@@ -2,6 +2,9 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var Boid = /** @class */ (function () {
+    /**
+     * Boid represents a "bird like object" that engages in flocking behavior with other boids.
+     */
     function Boid(x, y, canvasOpts, opts) {
         var _a = opts || {}, vx = _a.vx, vy = _a.vy, size = _a.size, color = _a.color;
         this.x = x;
@@ -12,6 +15,10 @@ var Boid = /** @class */ (function () {
         this.size = size || 10;
         this.color = color || "#000000";
     }
+    /**
+     * Draws the current point
+     * @param ctx The canvas rendering context.
+     */
     Boid.prototype.draw = function (ctx) {
         // Creates a circle
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
@@ -19,10 +26,20 @@ var Boid = /** @class */ (function () {
         ctx.closePath();
         ctx.fill();
     };
+    /** Updates poistion of current boid
+     * @param dt The difference, in milliseconds, since last update
+     * @param neighbors An array of neighbor boids
+     *
+     */
     Boid.prototype.update = function (dt, neighbors) {
         var _this = this;
         var _a = this.canvasOpts, width = _a.width, height = _a.height, closeDist = _a.closeDist;
+        // Separate from close neighbors
         this.separate(neighbors.filter(function (boid) { return dist(_this, boid) < closeDist; }));
+        // Align with neighbors
+        this.align(neighbors);
+        // Clamp between min and max speed
+        this.clampSpeed();
         var timeFactor = dt / 1000;
         if (!this.isCollided(width, height, this.vx * timeFactor, this.vy * timeFactor)) {
             this.x += this.vx * dt / 1000;
@@ -72,13 +89,48 @@ var Boid = /** @class */ (function () {
         this.vx += dx * avoidFactor;
         this.vy += dy * avoidFactor;
     };
+    Boid.prototype.align = function (neighbors) {
+        var _a = [0, 0, 0], vx_avg = _a[0], vy_avg = _a[1], n = _a[2];
+        var alignFactor = this.canvasOpts.alignFactor;
+        for (var i = 0; i < neighbors.length; i++) {
+            vx_avg += neighbors[i].vx;
+            vy_avg += neighbors[i].vy;
+            n++;
+        }
+        if (n > 0) {
+            vx_avg = vx_avg / n;
+            vy_avg = vy_avg / n;
+        }
+        this.vx += (vx_avg - this.vx) * alignFactor;
+        this.vy += (vy_avg - this.vy) * alignFactor;
+    };
+    // Clamps speed to limits
+    Boid.prototype.clampSpeed = function () {
+        var _a = this.canvasOpts, maxSpeed = _a.maxSpeed, minSpeed = _a.minSpeed;
+        var speed = Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2));
+        if (speed > maxSpeed) {
+            this.vx = (this.vx / speed) * maxSpeed;
+            this.vy = (this.vy / speed) * maxSpeed;
+        }
+        if (speed < minSpeed) {
+            this.vx = (this.vx / speed) * minSpeed;
+            this.vy = (this.vy / speed) * minSpeed;
+        }
+    };
     return Boid;
 }());
+/** Returns the Euclidean distance between two points in 2D space
+ * @param p1 A 2D Cartesian point {x, y}
+ * @param p2 A 2D Cartesian point {x, y}
+ * @return The distance (in same units as input) between p1 and p2
+ */
 function dist(p1, p2) {
     return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
 }
+// boid list global object
+// and global canvas options
 var boids = [];
-var canvasOpts = { width: canvas.width, height: canvas.height, avoidFactor: 0.1, neighborDist: 100, closeDist: 50 };
+var canvasOpts = { width: canvas.width, height: canvas.height, avoidFactor: 0.1, alignFactor: 0.01, neighborDist: 100, closeDist: 50, minSpeed: 30, maxSpeed: 100 };
 /**
  * Initializes the canvas. Should only run on page load.
  */
@@ -87,13 +139,14 @@ function init() {
         return;
     // Create 10 boids
     for (var i = 0; i < 10; i++) {
-        var boid = new Boid(Math.random() * (canvas.width - 20) + 10, Math.random() * (canvas.height - 20) + 10, canvasOpts, { vx: Math.random() * 400 - 200, vy: Math.random() * 400 - 200 });
+        var boid = new Boid(Math.random() * (canvas.width - 20) + 10, Math.random() * (canvas.height - 20) + 10, canvasOpts, { vx: Math.random() * 2 * canvasOpts.maxSpeed - canvasOpts.maxSpeed, vy: Math.random() * 2 * canvasOpts.maxSpeed - canvasOpts.maxSpeed });
         boids.push(boid);
         boid.draw(ctx);
     }
 }
 var previousTimestamp;
-/** Creates a step of animation
+/** Creates a step of animation. Will request another animation frame when done with current step.
+ * @param timestamp The current animation timestamp in milliseconds
  */
 function step(timestamp) {
     if (ctx == null)
@@ -102,20 +155,17 @@ function step(timestamp) {
         previousTimestamp = timestamp;
     }
     var dt = timestamp - previousTimestamp; // dt in milliseconds
-    // Skip animation if longer than 0.3s, implies 
-    // tab was closed
-    if (dt < 100) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.beginPath();
-        boids.forEach(function (boid) {
-            var neighbors = boids.filter(function (neighbor) { return dist(boid, neighbor) < canvasOpts.neighborDist; });
-            boid.update(dt, neighbors);
-            boid.draw(ctx);
-        });
-    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    boids.forEach(function (boid) {
+        var neighbors = boids.filter(function (neighbor) { return neighbor !== boid && (dist(boid, neighbor) < canvasOpts.neighborDist); });
+        boid.update(dt, neighbors);
+        boid.draw(ctx);
+    });
     previousTimestamp = timestamp;
     window.requestAnimationFrame(step);
 }
+// Intialize canvas and run animation
 window.onload = function () {
     init();
     window.requestAnimationFrame(step);
--- boids.ts
+++ boids.ts
@@ -3,20 +3,18 @@
 const canvas : HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-
-interface Drawable {
-	draw: (ctx: CanvasRenderingContext2D) => void
-}	
-
 type CanvasOpts = {
 	width: number
 	height: number
 	avoidFactor: number
+	alignFactor: number
 	neighborDist: number
 	closeDist: number
+	minSpeed: number
+	maxSpeed: number
 }
 
-class Boid implements Drawable {
+class Boid {
 	x: number
 	y: number
 	vx: number
@@ -24,6 +22,9 @@ class Boid implements Drawable {
 	private size: number
 	private color: string
 	private canvasOpts: CanvasOpts
+	/**
+	 * Boid represents a "bird like object" that engages in flocking behavior with other boids.
+	 */
 	constructor(x: number, y: number, canvasOpts: CanvasOpts, opts?: {vx?: number, vy?: number, size?: number, color?: string} ) {
 		const {vx, vy, size, color} = opts || {};
 		this.x = x;
@@ -35,6 +36,10 @@ class Boid implements Drawable {
 		this.color = color || "#000000";
 	}
 
+	/**
+	 * Draws the current point
+	 * @param ctx The canvas rendering context.
+	 */
 	draw(ctx: CanvasRenderingContext2D): void {
 		// Creates a circle
 		ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
@@ -43,9 +48,19 @@ class Boid implements Drawable {
 		ctx.fill();
 	}
 
+	/** Updates poistion of current boid
+	 * @param dt The difference, in milliseconds, since last update
+	 * @param neighbors An array of neighbor boids
+	 * 
+	 */
 	update(dt: number, neighbors: Boid[]): void {
 		const {width, height, closeDist} = this.canvasOpts;
+		// Separate from close neighbors
 		this.separate(neighbors.filter(boid => dist(this, boid) < closeDist));
+		// Align with neighbors
+		this.align(neighbors);
+		// Clamp between min and max speed
+		this.clampSpeed();
 		const timeFactor = dt/1000;
 		if (!this.isCollided(width, height, this.vx*timeFactor, this.vy*timeFactor)) {
 			this.x += this.vx * dt/1000;
@@ -88,7 +103,7 @@ class Boid implements Drawable {
 		return collided;
 	}
 
-	private separate(closeNeighbors: Boid[]) {
+	private separate(closeNeighbors: Boid[]):void {
 		let [dx, dy] = [0, 0];
 		const {avoidFactor} = this.canvasOpts;
 
@@ -100,14 +115,53 @@ class Boid implements Drawable {
 		this.vx += dx*avoidFactor;
 		this.vy += dy*avoidFactor;
 	}
+
+	private align(neighbors: Boid[]):void {
+		let [vx_avg, vy_avg, n] = [0, 0, 0];
+		const {alignFactor} = this.canvasOpts;
+		for (let i = 0; i < neighbors.length; i++) {
+			vx_avg += neighbors[i].vx;
+			vy_avg += neighbors[i].vy;
+			n++;
+		}
+		if (n > 0) {
+			vx_avg = vx_avg/n;
+			vy_avg = vy_avg/n;
+		}
+
+		this.vx += (vx_avg - this.vx)*alignFactor;
+		this.vy += (vy_avg - this.vy)*alignFactor;
+	}
+
+	// Clamps speed to limits
+	private clampSpeed(): void {
+		const {maxSpeed, minSpeed} = this.canvasOpts;
+		const speed = Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2));
+		if (speed > maxSpeed) {
+			this.vx = (this.vx/speed)*maxSpeed;
+			this.vy = (this.vy/speed)*maxSpeed;
+		}
+		if (speed < minSpeed) {
+			this.vx = (this.vx/speed)*minSpeed;
+			this.vy = (this.vy/speed)*minSpeed;
+		}
+	}
 }  
 
+/** Returns the Euclidean distance between two points in 2D space
+ * @param p1 A 2D Cartesian point {x, y} 
+ * @param p2 A 2D Cartesian point {x, y}
+ * @return The distance (in same units as input) between p1 and p2
+ */
 function dist(p1: {x: number, y: number}, p2: {x: number, y: number}): number {
 	return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
 }
 
+// boid list global object
+// and global canvas options
 const boids: Boid[] = [];
-const canvasOpts = {width: canvas.width, height: canvas.height, avoidFactor: 0.1, neighborDist: 100, closeDist: 50};
+const canvasOpts: CanvasOpts = {width: canvas.width, height: canvas.height, avoidFactor: 0.1, alignFactor: 0.01, neighborDist: 100, closeDist: 50, minSpeed: 30, maxSpeed: 100};
+
 /**
  * Initializes the canvas. Should only run on page load.
  */
@@ -115,7 +169,7 @@ function init() : void {
 	if (canvas == null || ctx == null) return;
 	// Create 10 boids
 	for (let i = 0; i < 10; i++) {
-		const boid = new Boid(Math.random()*(canvas.width - 20) + 10, Math.random()*(canvas.height - 20) + 10, canvasOpts, {vx: Math.random()*400 - 200, vy: Math.random()*400 - 200}); 
+		const boid = new Boid(Math.random()*(canvas.width - 20) + 10, Math.random()*(canvas.height - 20) + 10, canvasOpts, {vx: Math.random()*2*canvasOpts.maxSpeed - canvasOpts.maxSpeed, vy: Math.random()*2*canvasOpts.maxSpeed - canvasOpts.maxSpeed}); 
 		boids.push(boid);
 		boid.draw(ctx);
 	} 
@@ -123,7 +177,9 @@ function init() : void {
 }
 
 let previousTimestamp : number | undefined;
-/** Creates a step of animation
+
+/** Creates a step of animation. Will request another animation frame when done with current step.
+ * @param timestamp The current animation timestamp in milliseconds
  */
 function step(timestamp: number) : void {
 	if (ctx == null) return;
@@ -133,24 +189,22 @@ function step(timestamp: number) : void {
 	}
 
 	const dt = timestamp - previousTimestamp // dt in milliseconds
-	// Skip animation if longer than 0.3s, implies 
-	// tab was closed
-	if (dt < 100) {
-		ctx.clearRect(0,0, canvas.width, canvas.height);
-		ctx.beginPath();
 
-		boids.forEach(boid => {
-			const neighbors = boids.filter(neighbor => dist(boid, neighbor) < canvasOpts.neighborDist);
-			boid.update(dt, neighbors);
-			boid.draw(ctx);
-		});
-	}
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+	ctx.beginPath();
+
+	boids.forEach(boid => {
+		const neighbors = boids.filter(neighbor => neighbor !== boid && (dist(boid, neighbor) < canvasOpts.neighborDist));
+		boid.update(dt, neighbors);
+		boid.draw(ctx);
+	});
 
 	previousTimestamp = timestamp;
 	
 	window.requestAnimationFrame(step);
 } 
 
+// Intialize canvas and run animation
 window.onload = () => {
 	init();
 	window.requestAnimationFrame(step);
